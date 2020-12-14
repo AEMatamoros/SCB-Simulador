d41d608c11e003d7ed886b5ade1277a25367c778
Alexis <Alexismatamoros1996888@gmail.com>
2020-12-14 16:10:53 -0600

    Ahi hice cosas XD

--- BacProyect/js/windowController.js
+++ BacProyect/js/windowController.js
@@ -3,6 +3,7 @@ const path = require('path');
 const url = require('url');
 
 const newDataWindowBtn= document.getElementById('btn-data');
+const newDataWindowBtn2= document.getElementById('btn-learn');
 
 newDataWindowBtn.addEventListener('click', (event)=>{
     let dataWindow = new BrowserWindow({
@@ -21,3 +22,21 @@ newDataWindowBtn.addEventListener('click', (event)=>{
     }))
 })
 
+newDataWindowBtn2.addEventListener('click', (event)=>{
+    let dataWindow = new BrowserWindow({
+        webPreferences:{
+            nodeIntegration:true,
+            enableRemoteModule:true
+        },
+        width:'900px',
+        height:'900px'
+    });
+
+    //dataWindow.webContents.openDevTools();
+    dataWindow.removeMenu();
+    dataWindow.loadURL(url.format({
+        pathname:path.join("../templates/learn.html"),
+        protocol:false,
+        slashes:true
+    }))
+})
